b8f09190f5878dca987fe550605a569a134c53df
RRaveendran <RRaveendran@LT-RR>
2024-08-17 23:26:58 +0530

    adding files

---
+++ MyTODO/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyTODO
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
